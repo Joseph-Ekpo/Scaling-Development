560718e1a266ac8a37585b3b289bf8d5a89e1473
Joseph <31547996+Joseph-Ekpo@users.noreply.github.com>
2025-08-19 02:53:24 +0000

    Basic Python Class with some behaviour

--- PythonRefresherOOP/pro.py
+++ PythonRefresherOOP/pro.py
@@ -1,35 +1,55 @@
 # Leet Code 1: Two Sum
 
 # Task: Build a Car class with attributes (make, model) and method drive().
- # car sizes -->
-    # small = 1
-    # medium = 2
-    # big = 3
-    # 
-
-class ParkingLot:
-    
-    def __init__(self, spaces):
-        # spaces --> [small, medium, big]
-        self.spaces = spaces
-
+from lorem_text import lorem
+import time
 
 class Car:
    
-    def __init__(self, make, model, size):
+    def __init__(self, make, model, starting_gas):
         self.make = make
         self.model = model
-        self.size = size
+        self.fuel_capacity = starting_gas
+        self.parked = True
     
     def __str__(self):
-        return f"Make: {self.make}\nModel: {self.model}\n Size: {self.size}"
+        return f"Make: {self.make}\nModel: {self.model}\n Fuel: {self.fuel_capacity} gallons\nParked: {self.parked}"
     
-    def drive(self):
-        pass
+    def drive(self, location):
+        if self.parked:
+            print("Cannot drive when parked. Start car first")
+            return False
+        else:
+            print(f"Driving to: {location}. Consuming 1 fuel")
+            self.fuel_capacity -= 1
+            time.sleep(1)
+            print(f"Arrived and parked at{location}..")
+            self.parked = True
+        return self.parked
 
-    def park(self, lot) --> bool:
-        pass
+    def park(self):
+        self.parked = True
+        return f"Parked the {self.make}in the cool shade!"
+    
+    def start_car(self):
+        self.parked = False
+        return f"{self.model} {self.make} is ready to rock-n-roll"
+    
+    def honk_horn(self):
+        # noise = lorem.words(amount) if amount >= 10 else lorem.sentence(amount)
+        noise = lorem.words(5)
+        return noise
 
 
-honda = Car('honda', 'civic', 1)
+
+honda = Car('Honda', 'civic', 12.56)
+print(honda, "\n")
+
+honda.start_car()
+honda.drive("Los Angeles, CA")
+print(honda, "\n")
+
+
+honda.honk_horn()
+
 
