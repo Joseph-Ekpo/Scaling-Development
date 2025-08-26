aaa2e94da427db067db3a3e5260bb277974f2fc6
Joseph <31547996+Joseph-Ekpo@users.noreply.github.com>
2025-08-26 02:53:55 +0000

    Initial code upload for array traversal challenge

---
+++ 6-Arrays/arrays.py
@@ -0,0 +1,12 @@
+# Write a class that has a method called max_profit(earning) and 
+# provides the maximum earning.
+
+class ProfitCalculator:
+
+    def __init__(self, reciept: str):
+        self.reciept = reciept
+
+    def max_profit(self, earnings: list) -> int:
+        maximum: int = 0
+
+        return maximum
