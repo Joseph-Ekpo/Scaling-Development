7d9dc28be2d394c799199a0f746c96bdec27b6b1
Joseph <31547996+Joseph-Ekpo@users.noreply.github.com>
2025-08-26 02:15:57 +0000

    Updated iterator/generator code from book challenge

--- 4-IteratorGenerator/itgen.py
+++ 4-IteratorGenerator/itgen.py
@@ -1,6 +1,6 @@
 # https://github.com/realpython/materials/blob/master/generators/techcrunch.csv
 
-# It’s time to do some processing in Python! To demonstrate how to build pipelines with generators, you’re going to analyze this file to get the total and average of all series A rounds in the dataset.
+# Analyze the file to get the total and average of all series A rounds in the dataset.
 
 # Let’s think of a strategy:
 
@@ -11,10 +11,45 @@
 # Filter out the rounds you aren’t interested in.
 # Calculate the total and average values for the rounds you are interested in.
 
-# To dig even deeper, try figuring out the average amount raised per company in a series A round. This is a bit trickier, so here are some hints:
+# Figure out the average amount raised per company in a series A round.
 
-# Generators exhaust themselves after being iterated over fully.
-# You will still need the sum() function.
+file_name = "/workspaces/Scaling-Development/4-IteratorGenerator/techcrunch_iterators_generators.csv"
+
+lines = (line for line in open(file_name))
+list_line = (s.rstrip().split(",") for s in lines)
+cols = next(list_line)
+
+# for i in range(10):
+#     print(next(list_line))
+
+company_dicts = (dict(zip(cols, data)) for data in list_line)
+
+funding = (
+    int(company_dict["raisedAmt"])
+    for company_dict in company_dicts
+    if company_dict["round"] == "a"
+)
+total_series_a = sum(funding)
+print(f"Total series A fundraising: ${total_series_a}\n")
+
+
+# Figure out the average amount raised per company in a series A round. 
+company_series_a_funding_data = {}
+lines = (line for line in open(file_name))
+list_line = (s.rstrip().split(",") for s in lines)
+cols = next(list_line)
+company_dicts = (dict(zip(cols, data)) for data in list_line)
+
+for company_dict in company_dicts:
+    if company_dict['company'] not in company_series_a_funding_data and company_dict['round'] == 'a':
+        company_series_a_funding_data[company_dict['company']] = int(company_dict['raisedAmt'])
+
+average = sum(company_series_a_funding_data.values()) // len(company_series_a_funding_data)
+for company, amount_raised in company_series_a_funding_data.items():
+    print(f"Company: {company}\nSeries A Funding: ${amount_raised}\n")
+
+print()
+print(f"Average amount raised per company: ${average}")
 
 
 # Mini Task
@@ -27,8 +62,14 @@ class EvenYield:
 
 
     def even_numbers(self, n):
-        pass
+        for i in range(n):
+            yield i
+            i += 1
+            
 
 
-new_yielder = EvenYield()
-print(new_yielder.even_numbers(10))
+yielder = EvenYield()
+numbers = yielder.even_numbers(10)
+
+for num in numbers:
+    print(num)
