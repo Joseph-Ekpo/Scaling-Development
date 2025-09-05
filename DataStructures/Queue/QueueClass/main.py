70ab6a38cde2a3e6fb103348f08af75504a5ce8c
Joseph <31547996+Joseph-Ekpo@users.noreply.github.com>
2025-09-05 04:50:08 +0000

    Initial code template for Queue exercises

---
+++ DataStructures/Queue/QueueClass/main.py
@@ -0,0 +1,22 @@
+# Implement the following operations on the Queue class:
+
+# queue.push(item): Adds an item to the tail of the queue (index 0 of list)
+# queue.pop(): Removes and returns an item from the head of the queue (last index of list)
+# queue.peek(): Returns an item from the head of the queue
+# queue.size(): Returns the number of items in the queue
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        pass
+
+    def pop(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def size(self):
+        pass
