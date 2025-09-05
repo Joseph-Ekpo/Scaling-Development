a40f3a03814cfccb994497b6b1dbd3ff74bb2af2
Joseph <31547996+Joseph-Ekpo@users.noreply.github.com>
2025-09-05 00:22:18 +0000

    Ran test on updated code

---
+++ Algorithms/Sorting/QuickSort/main.py
@@ -0,0 +1,14 @@
+# Pseudocode
+# Select a "pivot" element - We'll arbitrarily choose the last element in the list
+# Move through all the elements in the list and swap them around until all the numbers less than the pivot are on the left, 
+# and the numbers greater than the pivot are on the right
+
+# Move the pivot between the two sections where it belongs
+# Recursively repeat for both sections
+
+def quick_sort(nums, low, high):
+    pass
+
+
+def partition(nums, low, high):
+    pass
