3c7c9619b05a33968eb2ec454a21066dc2494f15
Joseph <31547996+Joseph-Ekpo@users.noreply.github.com>
2025-09-09 15:22:35 +0000

    Implented LL Head code and fixed Add to tail description

--- DataStructures/LinkedLists/LinkedListAddTail/main.py
+++ DataStructures/LinkedLists/LinkedListAddTail/main.py
@@ -1,3 +1,10 @@
+# Complete the add_to_tail method. It adds a new node to the end of the list and returns nothing.
+
+# If there isn't a head node, set the new node as the head and be done.
+# Otherwise, keep a reference to the "last" node in the list - start with it set to None.
+# Iterate over the linked list (we built this already!)
+# Update your "last" node reference to the current node
+# Once you've iterated over the entire list, your "last" reference should be the last node in the list (the "tail"). Set the next field of the "last" node to the new node.
 from node import Node
 
 
